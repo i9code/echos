ab40141a6fdfd3ca901808259f1ada27985cdd48
storezhang <storezhang@gmail.com>
2020-02-11 15:09:29 +0800

    初始化

---
+++ echo.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+var (
+	DefaultEchoConfig = &EchoConfig{
+		Ip:           "",
+		Port:         1323,
+		Validate:     true,
+		ErrorHandler: true,
+		Init:         nil,
+		Routes:       nil,
+	}
+)
+
+type EchoConfig struct {
+	Ip           string
+	Port         int
+	Validate     bool
+	ErrorHandler bool
+	Init         func(e *echo.Echo)
+	Routes       []func(e *echo.Echo)
+}
+
+func (ec *EchoConfig) Address() string {
+	var address string
+	if "" != strings.TrimSpace(ec.Ip) {
+		address = fmt.Sprintf("%s:%d", ec.Ip, ec.Port)
+	} else {
+		address = fmt.Sprintf(":%d", ec.Port)
+	}
+
+	return address
+}
+
+func Start() {
+	StartWith(DefaultEchoConfig)
+}
+
+func StartWith(ec *EchoConfig) {
+	// 创建Echo对象
+	e := echo.New()
+
+	if nil != ec.Init {
+		ec.Init(e)
+	}
+	if nil != ec.Routes {
+		for _, route := range ec.Routes {
+			route(e)
+		}
+	}
+
+	// 初始化Validator
+	if ec.Validate {
+		initValidate()
+		// 数据验证
+		e.Validator = &customValidator{validator: v}
+	}
+
+	// 处理错误
+	if ec.ErrorHandler {
+		e.HTTPErrorHandler = func(err error, c echo.Context) {
+			type response struct {
+				Msg       string                                 `json:"msg"`
+				Validates validator.ValidationErrorsTranslations `json:"validates"`
+			}
+			rsp := response{}
+
+			code := http.StatusInternalServerError
+			switch re := err.(type) {
+			case *echo.HTTPError:
+				code = re.Code
+				rsp.Msg = re.Error()
+			case validator.ValidationErrors:
+				lang := c.Request().Header.Get("Accept-Language")
+				rsp.Validates = i18n(lang, re)
+			}
+
+			c.JSON(code, rsp)
+			c.Logger().Error(err)
+		}
+	}
+
+	// 初始化中间件
+	e.Pre(middleware.MethodOverride())
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	// e.Use(middleware.CSRF())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.RequestID())
+
+	// 启动Server
+	go func() {
+		if err := e.Start(ec.Address()); nil != err {
+			e.Logger.Fatal(err)
+		}
+	}()
+
+	// 等待系统退出中断并响应
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); nil != err {
+		e.Logger.Fatal(err)
+	}
+}
+
+func Int64Param(c echo.Context, name string) (int64, error) {
+	return strconv.ParseInt(c.Param(name), 10, 64)
+}
+
+func IntParam(c echo.Context, name string) (int, error) {
+	return strconv.Atoi(c.Param(name))
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/storezhang/gos
+
+go 1.12
+
+require (
+	github.com/casbin/casbin v1.9.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
+	github.com/go-playground/validator/v10 v10.1.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20200210222208-86ce3cb69678 // indirect
+)
