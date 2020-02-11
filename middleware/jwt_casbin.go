0dbd65df0d5f09a6f5fa5400be55a0fafdd208fe
storezhang <storezhang@gmail.com>
2020-02-11 15:10:09 +0800

    初始化

--- middleware/jwt_casbin.go
+++ middleware/jwt_casbin.go
@@ -1,65 +1,65 @@
 package middleware
 
 import (
-	"strings"
+    "strings"
 
-	"github.com/casbin/casbin"
+    "github.com/casbin/casbin"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
 )
 
 type (
-	Config struct {
-		Skipper    middleware.Skipper
-		Enforcer   *casbin.Enforcer
-		DataSource DataSource
-	}
+    Config struct {
+        Skipper    middleware.Skipper
+        Enforcer   *casbin.Enforcer
+        DataSource DataSource
+    }
 )
 
 var (
-	DefaultConfig = Config{
-		Skipper: middleware.DefaultSkipper,
-	}
+    DefaultConfig = Config{
+        Skipper: middleware.DefaultSkipper,
+    }
 )
 
 type DataSource interface {
-	GetUsernameByToken(token string) string
+    GetUsernameByToken(token string) string
 }
 
 func JWTMiddleware(e *casbin.Enforcer, ds DataSource) echo.MiddlewareFunc {
-	c := DefaultConfig
-	c.Enforcer = e
-	c.DataSource = ds
+    c := DefaultConfig
+    c.Enforcer = e
+    c.DataSource = ds
 
-	return JWTCasbinWithConfig(c)
+    return JWTCasbinWithConfig(c)
 }
 
 func JWTCasbinWithConfig(config Config) echo.MiddlewareFunc {
-	if config.Skipper == nil {
-		config.Skipper = DefaultConfig.Skipper
-	}
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			if config.Skipper(c) || config.CheckPermission(c) {
-				return next(c)
-			}
-			return echo.ErrForbidden
-		}
-	}
+    if config.Skipper == nil {
+        config.Skipper = DefaultConfig.Skipper
+    }
+    return func(next echo.HandlerFunc) echo.HandlerFunc {
+        return func(c echo.Context) error {
+            if config.Skipper(c) || config.CheckPermission(c) {
+                return next(c)
+            }
+            return echo.ErrForbidden
+        }
+    }
 }
 
 func (a *Config) GetUsername(c echo.Context) string {
-	token := c.Request().Header.Get("Authorization")
-	splitToken := strings.Split(token, "Bearer")
-	if 2 != len(splitToken) {
-		return ""
-	}
-	token = strings.TrimSpace(splitToken[1])
+    token := c.Request().Header.Get("Authorization")
+    splitToken := strings.Split(token, "Bearer")
+    if 2 != len(splitToken) {
+        return ""
+    }
+    token = strings.TrimSpace(splitToken[1])
 
-	return a.DataSource.GetUsernameByToken(token)
+    return a.DataSource.GetUsernameByToken(token)
 }
 
 func (a *Config) CheckPermission(c echo.Context) bool {
-	return a.Enforcer.Enforce(a.GetUsername(c), c.Request().URL.Path, c.Request().Method)
+    return a.Enforcer.Enforce(a.GetUsername(c), c.Request().URL.Path, c.Request().Method)
 }
--- validator.go
+++ validator.go
@@ -1,65 +1,65 @@
 package main
 
 import (
-	"strings"
+    "strings"
 
-	"github.com/go-playground/locales/en"
-	"github.com/go-playground/locales/zh"
-	ut "github.com/go-playground/universal-translator"
-	"github.com/go-playground/validator/v10"
-	enLang "github.com/go-playground/validator/v10/translations/en"
-	zhLang "github.com/go-playground/validator/v10/translations/zh"
+    "github.com/go-playground/locales/en"
+    "github.com/go-playground/locales/zh"
+    ut "github.com/go-playground/universal-translator"
+    "github.com/go-playground/validator/v10"
+    enLang "github.com/go-playground/validator/v10/translations/en"
+    zhLang "github.com/go-playground/validator/v10/translations/zh"
 )
 
 var (
-	v          *validator.Validate
-	translator *ut.UniversalTranslator
+    v          *validator.Validate
+    translator *ut.UniversalTranslator
 )
 
 type customValidator struct {
-	validator *validator.Validate
+    validator *validator.Validate
 }
 
 func (cv *customValidator) Validate(i interface{}) error {
-	return cv.validator.Struct(i)
+    return cv.validator.Struct(i)
 }
 
 func initValidate() {
-	v = validator.New()
+    v = validator.New()
 
-	translator = ut.New(en.New(), en.New(), zh.New())
-	if en, success := translator.GetTranslator("en"); success {
-		enLang.RegisterDefaultTranslations(v, en)
-	}
-	if zh, success := translator.GetTranslator("zh"); success {
-		zhLang.RegisterDefaultTranslations(v, zh)
-	}
+    translator = ut.New(en.New(), en.New(), zh.New())
+    if en, success := translator.GetTranslator("en"); success {
+        enLang.RegisterDefaultTranslations(v, en)
+    }
+    if zh, success := translator.GetTranslator("zh"); success {
+        zhLang.RegisterDefaultTranslations(v, zh)
+    }
 }
 
 func i18n(lang string, errs validator.ValidationErrors) (i18n validator.ValidationErrorsTranslations) {
-	sep := "_"
-	if strings.Contains(lang, "-") {
-		sep = "-"
-	}
+    sep := "_"
+    if strings.Contains(lang, "-") {
+        sep = "-"
+    }
 
-	splits := strings.Split(lang, sep)
-	for i := 0; i < len(splits); i++ {
-		if t, s := translator.FindTranslator(lang); s {
-			i18n = errs.Translate(t)
-			break
-		} else {
-			if index := strings.LastIndex(lang, sep); -1 == index {
-				break
-			} else {
-				lang = lang[0:index]
-			}
-		}
-	}
-	if nil == i18n {
-		if t, s := translator.GetTranslator("zh"); s {
-			i18n = errs.Translate(t)
-		}
-	}
+    splits := strings.Split(lang, sep)
+    for i := 0; i < len(splits); i++ {
+        if t, s := translator.FindTranslator(lang); s {
+            i18n = errs.Translate(t)
+            break
+        } else {
+            if index := strings.LastIndex(lang, sep); -1 == index {
+                break
+            } else {
+                lang = lang[0:index]
+            }
+        }
+    }
+    if nil == i18n {
+        if t, s := translator.GetTranslator("zh"); s {
+            i18n = errs.Translate(t)
+        }
+    }
 
-	return
+    return
 }
